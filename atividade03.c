b133e31d4d1823e4a4e8fafc9c7193468291cc91
andrearashiro <58425025+andrearashiro@users.noreply.github.com>
2022-06-17 17:05:37 -0300

    Update atividade03.c

--- atividade03.c
+++ atividade03.c
@@ -2,15 +2,28 @@
 #include <locale.h>
 #include <stdlib.h>
 
-void existeGado(numeroGado, matrizGado)
+//Subrotina para testar se o número do gado já foi digitado
+void existeGado(int numeroGado, int matrizGado[42])
 {
-    printf("O número do gado é %d", numeroGado);
-    printf("O número do pasto é %d", matrizGado);
+    int l;
+    
+	for (l=0; l<42; l++){
+    	if(numeroGado == matrizGado[l])
+		{
+    		printf("O número do gado já foi digitado! no número %d \n", l);
+    		system("pause");
+    		break;
+    		
+    		
+		}
+	}
 
 }
 
 int main()
 {
+    setlocale(LC_ALL,"Portuguese");
+	
     int par, impar,i,j,k, numberGado;
     par = 0;
     impar = 0;
@@ -23,34 +36,37 @@ int main()
     printf("Por favor digite o número do gado: \n");
     
 
-    for(i=1; i<10; i++){
+    for(i=0; i<42; i++){
        
         scanf("%d", &numberGado);
+        fflush(stdin);
         
             if((numberGado % 2) != 0){
-                existeGado(numberGado, gadoPar[i]);
-
+                
+		        existeGado(numberGado, gadoImpar); //Chamando a Subrotina para verificar se o gado já foi digitado
+				
                 printf("O gado %d vai  pro pasto ímpar \n", numberGado);
                              
                 gadoImpar[impar] = numberGado;
                 impar = impar + 1;
-                
+                                                
                 //printf("o i é %d, o par é %d \n", i, par);
                                 
             }else{
-                existeGado(numberGado, gadoImpar[i]);
-
+                
+		        existeGado(numberGado, gadoPar);
+				
                 printf("O gado %d vai pro pasto par \n", numberGado);
                            
                 gadoPar[par] = numberGado;
                 par = par + 1;
-                
-                
+                                              
                 //printf("o i é %d, o ímpar é %d \n", i, impar);
-                
-                
+                                
             }
     }
+    system("cls");
+    
     printf("Os gados pares são:");
     for (j=0; j<par; j++){
        printf("%d - ", gadoPar[j]);
@@ -59,8 +75,7 @@ int main()
    printf("\n Os gados ímpares são:");
     for (k=0; k<impar; k++){
        printf("%d - ", gadoImpar[k]);
-        //printf("\n Os gados ímpares são: %d -", gadoImpar[j]);
-   }
+    }
 
    
     
